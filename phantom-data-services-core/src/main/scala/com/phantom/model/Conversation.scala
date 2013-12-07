bf231ec603919c1dcc6fef2e74e83f0fa0a7ac48
Adam Parrish <aparrish@neosavvy.com>
2013-12-07 13:42:41 -0500

    Defining a conversation object.

---
+++ phantom-data-services-core/src/main/scala/com/phantom/model/Conversation.scala
@@ -0,0 +1,23 @@
+package com.phantom.model
+
+case class ConversationStarter(
+                                toUsers: List[Long],
+                                fromUser: Long,
+                                imageUrl: String,
+                                imageText: String
+                              )
+
+case class ConversationItem(id: Long,
+                            conversationId: Long,
+                            toUser: Long,
+                            fromUser: Long,
+                            imageUrl: String,
+                            imageText: String
+                            )
+
+case class ConversationSummary(mostRecent: ConversationItem)
+
+case class ConversationDetail(id: Long,
+                        conversationItems: List[ConversationItem])
+
+case class Feed(conversations: List[ConversationSummary])
