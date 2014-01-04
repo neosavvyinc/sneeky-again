948f2cd5705135786e9c2c3c8f86d974671c7d7d
Adam Parrish <aparrish@neosavvy.com>
2014-01-04 16:35:36 -0500

    Changing to use PhantomUser objects

--- phantom-data-services-core/src/main/scala/com/phantom/ds/framework/auth/PhantomRequestAuthenticator.scala
+++ phantom-data-services-core/src/main/scala/com/phantom/ds/framework/auth/PhantomRequestAuthenticator.scala
@@ -3,21 +3,21 @@ package com.phantom.ds.framework.auth
 import spray.routing.authentication.Authentication
 import spray.routing.{ AuthenticationFailedRejection, RequestContext }
 import scala.concurrent.{ ExecutionContext, Future, future }
-import com.phantom.model.User
 import com.phantom.ds.DSConfiguration
 import spray.routing.AuthenticationFailedRejection.CredentialsRejected
+import com.phantom.model.PhantomUser
 
 //For now this authenticator does a bit of both authentication and authorziation
 //since we have no real roles or permissioning yet..just being a user opens up all doors
 //hence, for every request, we opted for just one authenticator which we could use to identify a user
 trait RequestAuthenticator extends Authenticator {
-  def request(ctx : RequestContext)(implicit ec : ExecutionContext) : Future[Authentication[User]]
+  def request(ctx : RequestContext)(implicit ec : ExecutionContext) : Future[Authentication[PhantomUser]]
 }
 
 trait PhantomRequestAuthenticator extends RequestAuthenticator with DSConfiguration {
   sessionRepo : SessionRepository =>
 
-  def request(ctx : RequestContext)(implicit ec : ExecutionContext) : Future[Authentication[User]] = {
+  def request(ctx : RequestContext)(implicit ec : ExecutionContext) : Future[Authentication[PhantomUser]] = {
 
     future {
       val result = for {
@@ -40,14 +40,14 @@ trait PhantomRequestAuthenticator extends RequestAuthenticator with DSConfigurat
     }
   }
 
-  private def validateSession(sessionId : String) : Option[User] = {
+  private def validateSession(sessionId : String) : Option[PhantomUser] = {
     getUser(sessionId)
   }
 }
 
 trait PassThroughRequestAuthenticator extends PhantomRequestAuthenticator with MockSessionRepository {
 
-  override def request(ctx : RequestContext)(implicit ec : ExecutionContext) : Future[Authentication[User]] = {
+  override def request(ctx : RequestContext)(implicit ec : ExecutionContext) : Future[Authentication[PhantomUser]] = {
     Future.successful(getUser("").toRight(AuthenticationFailedRejection(CredentialsRejected, Nil)))
   }
 
--- phantom-data-services-core/src/main/scala/com/phantom/ds/framework/auth/SessionRepository.scala
+++ phantom-data-services-core/src/main/scala/com/phantom/ds/framework/auth/SessionRepository.scala
@@ -1,23 +1,23 @@
 package com.phantom.ds.framework.auth
 
-import com.phantom.model.User
+import com.phantom.model.{ PhantomUser, User }
 import scala.concurrent.{ ExecutionContext, Future }
 import org.joda.time.{ DateTimeZone, LocalDate }
 
 trait SessionRepository {
 
-  def getUser(sessionId : String) : Option[User]
+  def getUser(sessionId : String) : Option[PhantomUser]
 
 }
 
 trait SqlSessionRepository extends SessionRepository {
 
-  def getUser(sessionId : String) : Option[User] = ???
+  def getUser(sessionId : String) : Option[PhantomUser] = ???
 
 }
 
 trait MockSessionRepository extends SessionRepository {
 
-  def getUser(sessionId : String) : Option[User] = Some(User(1L, "nsauro@sauron.com", LocalDate.now(DateTimeZone.UTC), true))
+  def getUser(sessionId : String) : Option[PhantomUser] = Some(PhantomUser(None, "nsauro@sauron.com", "12/21/2003", true, ""))
 
 }
