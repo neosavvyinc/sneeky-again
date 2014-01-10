c4d8fa3e8623c261d2e96a8e9c1a9b12354d3fa2
Adam Parrish <aparrish@neosavvy.com>
2014-01-09 20:42:32 -0500

    Adding some documentations

---
+++ phantom-data-services-core/src/test/scala/com/phantom/ds/dataAccess/ConversationItemDAOSpec.scala
@@ -0,0 +1,12 @@
+package com.phantom.ds.dataAccess
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aparrish
+ * Date: 1/9/14
+ * Time: 8:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ConversationItemDAOSpec {
+
+}
