a9be49ca6eb3da50557603e644b782995853b902
Adam Parrish <aparrish@neosavvy.com>
2014-02-01 13:02:45 -0500

    Little bit of debugging.

--- phantom-data-services-core/src/main/scala/com/phantom/ds/registration/UUIDExtractor.scala
+++ phantom-data-services-core/src/main/scala/com/phantom/ds/registration/UUIDExtractor.scala
@@ -3,14 +3,18 @@ package com.phantom.ds.registration
 import java.util.UUID
 import scala.util.Try
 import com.phantom.model.RegistrationVerification
+import com.phantom.ds.framework.Logging
 
-object UUIDExtractor {
+object UUIDExtractor extends Logging {
 
   private val regex = "##"
 
   def extractUUID(regVer : RegistrationVerification) : Option[UUID] = {
 
     val split : List[String] = regVer.body.split(regex).toList
+
+    log.debug(split.toString())
+
     split match {
       case x :: y :: z :: Nil => validateUUID(y)
       case _                  => None
