c5457bfe9213e88c4b1c043006e641a5364c75df
unicodesnowman <utfsnowman@gmail.com>
2014-02-08 17:38:08 -0500

    making APNS work

--- phantom-data-services-core/src/main/scala/com/phantom/ds/integration/apple/AppleActor.scala
+++ phantom-data-services-core/src/main/scala/com/phantom/ds/integration/apple/AppleActor.scala
@@ -9,12 +9,20 @@ import scala.util.Try
 import com.relayrides.pushy.apns._
 import util._
 
+class AppleAPNSRejectListener extends RejectedNotificationListener[SimpleApnsPushNotification] with Logging {
+  def handleRejectedNotification(notification : SimpleApnsPushNotification, reason : RejectedNotificationReason) = {
+    log.trace(s"received rejection notification")
+    log.trace(s"notification: $notification")
+    log.trace(s"reason: $reason")
+  }
+}
+
 trait AppleService {
   def pushManager : PushManager[SimpleApnsPushNotification]
 }
 
 object AppleService {
-  def apply(keyStorePassword : String, certificatePath : String) : AppleService = new AppleService {
+  def apply(keyStorePassword : String, certificatePath : String) : AppleService = new AppleService with Logging {
 
     private val keystoreInputStream = new java.io.FileInputStream(certificatePath)
 
@@ -37,12 +45,15 @@ object AppleService {
     //      case Success(s) => println("Success")
     //    }
     //
-    def pushManager = new PushManager[SimpleApnsPushNotification](
+    val pushManager = new PushManager[SimpleApnsPushNotification](
       ApnsEnvironment.getSandboxEnvironment(), // Production v. Sandbox
-      keyStore.getOrElse(throw new Exception()), // FIX THIS
+      keyStore.get, // FIX THIS
       keyStorePassword.toCharArray()
     )
 
+    private val listener = new AppleAPNSRejectListener()
+
+    pushManager.registerRejectedNotificationListener(listener)
     pushManager.start()
 
     // finally?
@@ -55,8 +66,7 @@ class AppleActor(service : AppleService) extends Actor with DSConfiguration with
   def receive : Actor.Receive = {
     case user : PhantomUser => {
       log.trace(s"received $user")
-      val tokenString = "6964897bb4b06ea805ae58b7821c3cfab77e34347785bd06f6f22d0e07dea176"
-      //val tokenString = "<5f6aa01d 8e335894 9b7c25d4 61bb78ad 740f4707 462c7eaf bebcf74f a5ddb387>"
+      val tokenString = "b4aa9a5aa1ac55ac0c038b8c55733e90b68290592ae1d76dd2d0837e38bfb0da" // chris
 
       val payloadBuilder = new ApnsPayloadBuilder()
       payloadBuilder.setBadgeNumber(1)
@@ -64,11 +74,11 @@ class AppleActor(service : AppleService) extends Actor with DSConfiguration with
       payloadBuilder.setSoundFileName("dicks-on-the-phone-rang-rang.aiff")
 
       val payload = payloadBuilder.buildWithDefaultMaximumLength()
+
       service.pushManager.enqueuePushNotification(
         new SimpleApnsPushNotification(TokenUtil.tokenStringToByteArray(tokenString), payload))
     }
   }
-
 }
 
 sealed trait AppleMessage
