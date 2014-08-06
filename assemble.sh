23f4023d3a99727c109ac6caf3648791e2b2c3fb
Adam Parrish <aparrish@neosavvy.com>
2014-08-06 08:12:20 -0400

    Saving our deploy approach.

--- assemble.sh
+++ assemble.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sbt clean assembly; cp shoutout-data-services-core/target/scala-2.10/shoutout.jar ../ShoutoutServers/playbooks/deploy-server-app/jar/shoutout.jar
+sbt clean assembly;
+cp shoutout-data-services-core/target/scala-2.10/shoutout.jar ./ansible/roles/deploy/files/
